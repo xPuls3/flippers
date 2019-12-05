9c030e0675cda4a031275c7dbca02583a3ab24d3
xPuls3 <Xer0.Puls3.XXII@gmail.com>
2019-12-04 23:12:10 -0500

    Add files via upload

---
+++ js/list.js
@@ -0,0 +1,33 @@
+const list = [
+
+    {
+        username: "Driztnar",
+        id: 1439,
+        date: "8/18/2019"
+    },
+
+    {
+        username: "Linquished",
+        id: 1225,
+        date: "8/18/2019"
+    },
+
+    {
+        username: "Vellu",
+        id: 3728,
+        date: "8/18/2019"
+    },
+
+    {
+        username: "Discoboy",
+        id: 2016,
+        date: "10/7/2019"
+    },
+
+    {
+        username: "haib993",
+        id: 2844,
+        date: "11/29/2019",
+    },
+
+];
